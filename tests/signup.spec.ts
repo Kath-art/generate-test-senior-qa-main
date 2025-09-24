8c457a22b0838e543ada44e3d89286bc31cd60f8
Kath <triggskatherine@gmail.com>
2025-09-24 14:31:43 +1200

    Add tests for: All required fields being empty. The 'happy path'

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -148,4 +148,49 @@ test.describe('Validation coverage where individual "required" fields are empty'
 
         expect(joinPage.dobWarnMsg).toBeVisible();
     });
+
+    test.describe('Validation where all required fields are empty', () => {
+        test('Show all warning messages when required fields are empty', async ({page}) => {
+            const joinPage = new JoinPage(page);
+            await joinPage.clickContinue();
+            expect(joinPage.dobWarnMsg).toBeVisible();
+            expect(joinPage.emailAddressWarningMsg).toBeVisible();
+            expect(joinPage.mobileNumberWarningMsg).toBeVisible();
+            expect(joinPage.firstNameWarnMessage).toBeVisible();
+            expect(joinPage.lastNameWarningMsg).toBeVisible();
+            expect(joinPage.productDiscStatementWarningMsg).toBeVisible();
+            expect(joinPage.privacyAndDeclarationWarningMsg).toBeVisible();
+        });
+    });
+
+    test.describe('Happy path - all fields are filled in correctly', () => {
+        test('Show all warning messages when required fields are empty', async ({page}) => {
+            const joinPage = new JoinPage(page);
+
+            await joinPage.selectTitle('Mrs');
+            await joinPage.selectGender('Female');
+            await joinPage.enterDOB('04/07/1934');
+            await joinPage.enterFirstName('Mildred');
+            await joinPage.enterMiddleName('Agatha');
+            await joinPage.enterLastName('Christie');
+            await joinPage.enterPreferredName('Author');
+            await joinPage.enterEmailAddress('triggskatherine@gmail.com');
+            await joinPage.enterMobileNumber('215249857');
+            await joinPage.checkProductDisclosure();
+            await joinPage.checkPrivacyAndDeclaration();
+            await joinPage.checkSmsConsent();
+            await joinPage.clickContinue();
+
+            await expect(joinPage.dobWarnMsg).not.toBeVisible();
+            await expect(joinPage.emailAddressWarningMsg).not.toBeVisible();
+            await expect(joinPage.mobileNumberWarningMsg).not.toBeVisible()
+            await expect(joinPage.firstNameWarnMessage).not.toBeVisible()
+            await expect(joinPage.lastNameWarningMsg).not.toBeVisible()
+            await expect(joinPage.productDiscStatementWarningMsg).not.toBeVisible()
+            await expect(joinPage.privacyAndDeclarationWarningMsg).not.toBeVisible()
+        });
+    });
+
+
+
 });
